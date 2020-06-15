aeed9ca67b035b0f08ff673bd5924fc2e9000d1c
Alanisia <2478196682@qq.com>
2020-06-15 16:57:32 +0800

    update

---
+++ src/algs.h
@@ -0,0 +1,12 @@
+#ifndef ALGS
+#define ALGS
+
+#include "struct.h"
+
+long horner(int x, int a[]); 
+long binary_search(int x, int l, int r, int m);
+void quick_sort(int a[],int l,int r,int m);
+int* merge_sort(int a[],int b[]);
+
+
+#endif
---
+++ src/util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL
+#define UTIL
+
+#include "struct.h"
+#include <stdio.h>
+
+typedef void (*print)(element);
+
+#endif
